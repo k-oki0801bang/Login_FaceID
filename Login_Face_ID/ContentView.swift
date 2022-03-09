b3539489d95aaa3f170230fd58b429c0b34249f5
Kouki <yamadakoki0124@icloud.com>
2022-03-09 22:28:53 +0900

    Initial Commit

---
+++ Login_Face_ID/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login_Face_ID
+//
+//  Created by 山田滉暁 on 2022/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Login_Face_ID/Login_Face_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_Face_IDApp.swift
+//  Login_Face_ID
+//
+//  Created by 山田滉暁 on 2022/03/09.
+//
+
+import SwiftUI
+
+@main
+struct Login_Face_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
